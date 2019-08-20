0344a1fc47efff18a1a08aa2095624a3ee21d737
tamunobelemamiebaka-ogan <belema@softcom.ng>
2019-08-20 15:02:31 +0100

    Add constraints to work to improve performance

--- app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
+++ app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
@@ -18,9 +18,8 @@
 package com.example.android.devbyteviewer
 
 import android.app.Application
-import androidx.work.ExistingPeriodicWorkPolicy
-import androidx.work.PeriodicWorkRequestBuilder
-import androidx.work.WorkManager
+import android.os.Build
+import androidx.work.*
 import com.example.android.devbyteviewer.work.RefreshDataWork
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -44,15 +43,25 @@ class DevByteApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
-        delayedInit()
+        setupRepeatingWork()
     }
 
-    private fun delayedInit() {
+    private fun setupRepeatingWork() {
         applicationScope.launch {
+            val constraints = Constraints.Builder()
+                    .setRequiredNetworkType(NetworkType.UNMETERED)
+                    .setRequiresBatteryNotLow(true)
+                    .setRequiresCharging(true)
+                    .apply {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            setRequiresDeviceIdle(true)
+                        }
+                    }.build()
+
             val repeatingWorkRequest = PeriodicWorkRequestBuilder<RefreshDataWork>(
-                    1,
-                    TimeUnit.DAYS
-            ).build()
+                    1, TimeUnit.DAYS)
+                    .setConstraints(constraints)
+                    .build()
 
             WorkManager.getInstance().enqueueUniquePeriodicWork(RefreshDataWork.WORK_NAME,
                     ExistingPeriodicWorkPolicy.KEEP,
